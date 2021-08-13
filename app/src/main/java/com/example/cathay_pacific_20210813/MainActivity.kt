af8dc772b9e50cb1e15e67266fe5b5211fd1b711
donghao <mikedong@ptc-nec.com.tw>
2021-08-14 00:29:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/cathay_pacific_20210813/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cathay_pacific_20210813
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
